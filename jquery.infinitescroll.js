c04b2f8a81f2ff80970d2183f7f5db7cc0e707ea
smathieu <simon.math@gmail.com>
2011-02-16 17:12:07 -0500

    Updated plugin to fit my needs:   - Expects a JSON response from the server   - Allows for a search field   - Look for scroll event so that it can be used with a control, not   just the document.

--- jquery.infinitescroll.js
+++ jquery.infinitescroll.js
@@ -6,36 +6,56 @@
             var settings = $.extend({
                     url: null,
                     triggerAt: 300,
-                    page: 2,
+                    page: 1,
                     appendTo: '.list tbody',
-                    container: $(document)
+                    force: true,
+                    container: '.list_container',
+                    search_field: "",
                 }, options);
             var req = null;
             var maxReached = false;
+            var was_searching = false;
 
             var infinityRunner = function() {
                 if (settings.url !== null) {
-                    if  (settings.force || (settings.triggerAt >= (settings.container.height() - el.height() - el.scrollTop()))) {
+                    if  (settings.force || (settings.triggerAt >= ($(settings.container).height() - el.height() - el.scrollTop()))) {
                         settings.force = false;
                         // if the request is in progress, exit and wait for it to finish
                         if (req && req.readyState < 4 && req.readyState > 0) {
                             return;
                         }
                         $(settings.appendTo).trigger('infinitescroll.beforesend');
-                        req = $.get(settings.url, 'page='+settings.page, function(data) {
-                            if (data !== '') {
-                                if (settings.page > 1) {
-                                    $(settings.appendTo).append(data);
-                                } else {
-                                    $(settings.appendTo).html(data);
-                                }
+
+
+                        params = {
+                          page: settings.page,
+                        };
+
+                        var search_field = $(settings.search_field);
+                        if (search_field.length == 1) {
+                          params['search_string'] = search_field.val();
+                          was_searching = true;
+                        } else if (was_searching) {
+                          // If we were searching and the search field has now been clear, reload from the beginning
+                          was_searching = false;
+                          params.page = 1;
+                          setting.page = 1;
+                        }
+
+                        req = $.get(settings.url, params, function(data) {
+                            if (data.append) {
+                                $(settings.appendTo).append(data.append);
                                 settings.page++;
                                 $(settings.appendTo).trigger('infinitescroll.finish');
-                            } else {
+                            } else if (data.replace) {
+                                $(settings.container).html(data.replace);
+                                settings.page = 2;
+                                $(settings.appendTo).trigger('infinitescroll.finish');
+                            } else if (data.finish) {
                                 maxReached = true;
                                 $(settings.appendTo).trigger('infinitescroll.maxreached');
                             }
-                        }, 'html');
+                        }, 'json');
                     }
                 }
             };
@@ -50,6 +70,11 @@
                 if (!maxReached) {
                     infinityRunner();
                 }
+            });
+                        
+            $(settings.search_field).keyup(function(e) {
+              settings.force = true;
+              infinityRunner();
             });
 
             // Test initial page layout for trigger
