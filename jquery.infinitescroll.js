d574a830c7137b209cae549d9e00e84747f0ff1d
smathieu <simon.math@gmail.com>
2011-02-17 12:53:47 -0500

    Fix some bugs related to search.

--- jquery.infinitescroll.js
+++ jquery.infinitescroll.js
@@ -15,6 +15,8 @@
             var req = null;
             var maxReached = false;
             var was_searching = false;
+            var last_search = null;
+            var more_stuff = false;
 
             var infinityRunner = function() {
                 if (settings.url !== null) {
@@ -22,35 +24,47 @@
                         settings.force = false;
                         // if the request is in progress, exit and wait for it to finish
                         if (req && req.readyState < 4 && req.readyState > 0) {
+                            more_stuff = true;
                             return;
                         }
                         $(settings.appendTo).trigger('infinitescroll.beforesend');
 
-
-                        params = {
-                          page: settings.page,
-                        };
+                        var params = {};
 
                         var search_field = $(settings.search_field);
-                        if (search_field.length == 1) {
-                          params['search_string'] = search_field.val();
+                        if (search_field.length == 1 && search_field.val().length > 0) {
+                          var search_s = search_field.val();
+
+                          if (search_s == last_search) {
+                            settings.page++;
+                          }
+                          last_search = search_s;
+                          params['search_string'] = search_s;
                           was_searching = true;
                         } else if (was_searching) {
                           // If we were searching and the search field has now been clear, reload from the beginning
                           was_searching = false;
-                          params.page = 1;
-                          setting.page = 1;
+                          last_search = null;
+                          settings.page = 1;
+                          $(settings.container).html("");
                         }
 
+                        params.page = settings.page
+
                         req = $.get(settings.url, params, function(data) {
+                            if (more_stuff) {
+                                more_stuff = false;
+                                infinityRunner();
+                                return;
+                            }
                             if (data.append) {
                                 $(settings.appendTo).append(data.append);
                                 settings.page++;
-                                $(settings.appendTo).trigger('infinitescroll.finish');
+                                $(settings.appendTo).trigger('infinitescroll.finish', data.append);
                             } else if (data.replace) {
                                 $(settings.container).html(data.replace);
                                 settings.page = 2;
-                                $(settings.appendTo).trigger('infinitescroll.finish');
+                                $(settings.appendTo).trigger('infinitescroll.finish', data.replace);
                             } else if (data.finish) {
                                 maxReached = true;
                                 $(settings.appendTo).trigger('infinitescroll.maxreached');
